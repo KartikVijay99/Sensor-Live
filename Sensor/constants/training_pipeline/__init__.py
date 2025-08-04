234bc76dd48403b8571afaca6b22d22156e65ba0
Kartik Vijay <kartikvijay9680@gmail.com>
2025-08-05 00:33:18 +0530

    Worked on data ingestion , entity folder config file and initialization of pipeline folder along with file.

---
+++ Sensor/constants/training_pipeline/__init__.py
@@ -0,0 +1,22 @@
+import os 
+
+TARGET_COLUMN = "class"
+PIPELINE_NAME = "sensor"
+ARTIFACT_DIR = "artifact"
+FILE_NAME = "sensor.csv"
+
+
+
+PREPROCSSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+MODEL_FILE_NAME = "model.pkl"
+
+
+SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
+SCHEMA_DROP_COLS = "drop_columns"
+
+
+"""
+ data  ingestion realted constant values 
+"""
+
+
